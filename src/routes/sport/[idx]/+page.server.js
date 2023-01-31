f0760e6d3fa74a7415b9379222b5ca242a3dea1b
wlankasper <asu.artur.smirnov@gmail.com>
2023-01-31 01:54:06 +0100

    beta sport section

--- src/routes/sport/[idx]/+page.server.js
+++ src/routes/sport/[idx]/+page.server.js
@@ -3,7 +3,7 @@ import contentfulFetch from '$lib/utils/util-contentful';
 const load = async (context) => {
     const { params } = context;
     const idx = params.idx;
-    const query = `
+    const querySport = `
     {
         sportSectionCollection (where:{
             sys: {
@@ -24,20 +24,83 @@ const load = async (context) => {
         }
     }
     `;
+    const responseSport = await contentfulFetch(querySport);
 
-    const response = await contentfulFetch(query);
-
-    if (!response.ok) {
+    if (!responseSport.ok) {
         throw error(404, {
             message: response.statusText,
         });
     }
 
-    const { data } = await response.json();
-    const { sportSectionCollection } = data;
+    const { data: sportData } = await responseSport.json();
+    const { sportSectionCollection } = sportData;
+
+    const queryPhoto = `
+    {
+        assetCollection (where: {
+            contentfulMetadata: {
+                tags: {
+                  id_contains_all: "photo${sportSectionCollection.items[0].title.replaceAll(' ', '')}"
+                }
+              }
+            })
+          {
+            items {
+              url
+            }
+          }
+    }
+    `;
+    const responsePhoto = await contentfulFetch(queryPhoto);
+
+    if (!responseSport.ok) {
+        throw error(404, {
+            message: response.statusText,
+        });
+    }
+
+    const { data: photoData } = await responsePhoto.json();
+    const { assetCollection } = photoData;
+
+    const queryNews = `
+    {
+        newsSectionCollection(where: { sportSection: { title: "${sportSectionCollection.items[0].title}" } },limit: 3) {
+            items{
+              sys {
+                id
+              }
+              sportSection {
+                title
+              }
+              date
+              text {
+                json
+              }
+              imagesFilesCollection(limit: 2) {
+                items {
+                  title
+                  url
+                }
+              }
+            }
+          }
+    }
+    `;
+    const responseNews = await contentfulFetch(queryNews);
+
+    if (!responseNews.ok) {
+        throw error(404, {
+            message: response.statusText,
+        });
+    }
+
+    const { data: newsData } = await responseNews.json();
+    const { newsSectionCollection } = newsData;
 
     return {
         sportSection: sportSectionCollection.items,
+        photoCollection: assetCollection.items,
+        newsCollection: newsSectionCollection.items,
     }
 };
 
--- src/routes/sport/[idx]/+page.svelte
+++ src/routes/sport/[idx]/+page.svelte
@@ -1,55 +1,62 @@
 <script>
+    import NewsPreview from "$lib/components/NewsPreview.svelte";
+
     export let data;
 
     const props = {
-        title: data.sportSection[0].title,
-        url: data.sportSection[0].imageHero.url,
-        description: data.sportSection[0].description,
+        sport: {
+            title: data.sportSection[0].title,
+            url: data.sportSection[0].imageHero.url,
+            description: data.sportSection[0].description,
+        },
+        photo: {
+            collection: data.photoCollection,
+        },
+        news: {
+            collection: data.newsCollection,
+        },
     };
-    console.log(data);
 </script>
 
 <section>
-    <div class="heading" style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.7) 100%), url({props.url});">
+    <div
+        class="heading"
+        style="background-image: linear-gradient(180deg, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.7) 100%), url({props
+            .sport.url});"
+    >
         <h1 class="text text-heading text-extend">
-            {props.title}
+            {props.sport.title}
         </h1>
     </div>
     <div class="container">
-        <div class="menu">
-            <a href="#description">
-                <h3>
-                    Descrizione
-                </h3>
-            </a>
-            <a href="#photo">
-                <h3>
-                    Photo
-                </h3>
-            </a>
-            <a href="#news">
-                <h3>
-                    News
-                </h3>
-            </a>
-        </div>
-        <div class="line-split" />
-        <div class="sport">
+        <div class="wrapper">
             <section id="description">
-                <h2 class="text">
-                    Descrizione
-                </h2>
-                <h4 class="text">
-                    {props.description}
+                <h2 class="text text-heading text-accent">Descrizione</h2>
+                <h4 class="text text-dercription">
+                    {props.sport.description}
                 </h4>
             </section>
             <section id="photo">
-
-            </section>
-            <section id="news">
-
+                <h2 class="text text-heading text-accent">Photo</h2>
+                <div class="photo-container">
+                    {#each props.photo.collection as photo}
+                        <div
+                            class="photo"
+                            style="background-image: url({photo.url});"
+                        />
+                    {/each}
+                </div>
             </section>
         </div>
+        <section id="news">
+            {#each props.news.collection as news}
+                <NewsPreview
+                    title={news.sportSection.title}
+                    date={news.date}
+                    text={news.text.json.content[0].content[0].value}
+                    href="/news/{news.sys.id}"
+                />{/each}
+        </section>
     </div>
 </section>
 
@@ -71,17 +78,82 @@
         display: flex;
         align-items: flex-start;
         flex-direction: row;
-        gap: 5rem;
+        gap: 6rem;
+
+        padding-top: 5vh;
     }
 
-    div.menu {
-        width: 20vw;
+    @media screen and (min-width: 100px) and (max-width: 1140px) {
+        div.container {
+            flex-direction: column;
+
+            padding: var(--padding-section);
+            padding-top: 5vh;
+        }
     }
 
-    div.line-split {
-        width: 1px;
-        height: 200px;
+    div.wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 4rem;
+    }
 
-        background-color: var(--line);
+    h2.text-heading {
+        margin-bottom: 1rem;
+    }
+
+    .text-dercription {
+        max-width: 40vw;
+    }
+
+    @media screen and (min-width: 100px) and (max-width: 1140px) {
+        .text-dercription {
+            max-width: 80vw;
+        }
+    }
+
+    div.photo-container {
+        max-width: 30vw;
+
+        display: flex;
+        flex-wrap: wrap;
+        gap: 0.5rem;
+    }
+
+    @media screen and (min-width: 100px) and (max-width: 1140px) {
+        div.photo-container {
+            max-width: 80vw;
+
+            display: grid;
+            grid-template-columns: repeat(2, minmax(0, 1fr));
+            gap: 5rem;
+        }
+    }
+
+    div.photo {
+        width: 200px;
+        height: 120px;
+
+        border-radius: 4px;
+
+        background-position: center;
+        background-size: cover;
+    }
+
+    @media screen and (min-width: 100px) and (max-width: 1140px) {
+        div.photo {
+        width: 40vw;
+        height: 15vh;
+
+        border-radius: 4px;
+
+        background-position: center;
+        background-size: cover;
+    }
+    }
+
+    section#news {
+        display: grid;
+        gap: 5rem 0;
     }
 </style>
