6043e944d36b96ab914f4ff669097f8e210648c9
wlankasper <asu.artur.smirnov@gmail.com>
2023-02-02 20:54:42 +0100

    error page / update font size on low media

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<section class="container">
+    <h1 class="text text-heading text-accent">
+        Opsss, qualcosa si e rotto 😅
+    </h1>
+    <h2 class="text text-heading">{$page.error.message}</h2>
+</section>
+
+<style>
+    section.container {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        padding: 30vh 5vw;
+    }
+
+    @media screen and (min-width: 100px) and (max-width: 1140px) {
+        section.container {
+            padding: 30vh 20vw;
+        }
+    }
+</style>
